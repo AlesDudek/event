b1beb5d85bccd1a3cda2e6e73854c9a8a449a46e
Aleš <mit.mit@seznam.cz>
2023-02-01 17:39:30 +0100

    treatment email enrollment

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const fullName = document.querySelector(".fullName");
 const fullEmail = document.querySelector(".fullEmail");
 const fullPhone = document.querySelector(".fullPhone");
 
+const textEmail = document.querySelector("#text__email");
+let pattern = /^([a-z\d\.]+)@([a-z\d\-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+
+
 const notificationName = document.querySelector(".notificationName");
 const notificationEmail = document.querySelector(".notificationEmail")
 const notificationPhone = document.querySelector(".notificationPhone")
@@ -38,5 +42,30 @@ form.addEventListener("submit", function(event){
     fullPhone.style.borderColor = "red"
     fullPhone.style.marginTop = "0"
   }
-  
+})
+
+fullEmail.addEventListener("input", function(){
+  let emailValue = fullEmail.value
+  if(emailValue.match(pattern)){
+    textEmail.textContent = "Your email is correct"
+    textEmail.style.color = "green"
+    textEmail.style.fontSize = "32px"
+    textEmail.style.marginTop = "24px"
+    fullEmail.style.marginTop = "0"
+    form.classList.add("valid")
+    form.classList.remove("invalid")
+  }else{
+    textEmail.textContent = "Your email is not correct"
+    textEmail.style.color = "red"
+    textEmail.style.fontSize = "32px"
+    textEmail.style.marginTop = "24px"
+    fullEmail.style.marginTop = "0"
+    form.classList.add("valid")
+    form.classList.remove("invalid")
+  }
+  if(emailValue === ""){
+    textEmail.textContent = ""
+    form.classList.add("valid")
+    form.classList.remove("invalid")
+  }
 })
